4f25181d92c1724db41c5a8b4d945b193f077a5f
mr-hritu <83459565+mr-hritu@users.noreply.github.com>
2023-06-11 09:13:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
-echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/ZauteKm/Instagram.git /Instagram
-cd /Instagram
+
 echo "Installing Requirements..."
 pip3 install -r requirements.txt
 echo "Starting Bot, Please Wait..."
